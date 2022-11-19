32bc42579c221087a9a74599cfda26bd2b39decf
Monosphere <103843418+Monosphere@users.noreply.github.com>
2022-11-20 00:26:23 +0800

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,130 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+namespace TommyGunMod
+{
+    public class Main : MelonMod
+    {
+        private string WeaponParentPath = "Player_Audrey(Clone)/Content/CameraPivot/HeadContainer/CameraContainer/GameCamera/ArmsContainer/Audrey@TPose/BN_Root001/BN_Pelvis001/BN_Spine_A001/BN_Spine_B001/BN_Spine_C001/BN_Spine_D001/BN_Chest001/BN_R_Clavicle001/BN_R_UpperArm001/BN_R_ForeArm001/BN_R_Hand001/BN_R_Prop/WeaponParent/";
+        Player player = null;
+        GunHandler gunHandler = null;
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            if (sceneName == "InitializeGame")
+            {
+                if (gunHandler == null)
+                {
+                    Debug.Log("Slayification");
+                    GameObject obj = new GameObject("GunManager");
+                    gunHandler = obj.AddComponent<GunHandler>();
+                }
+            }
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+    }
+    public class GunHandler : MonoBehaviour
+    {
+        private string WeaponParentPath = "Player_Audrey(Clone)/Content/CameraPivot/HeadContainer/CameraContainer/GameCamera/ArmsContainer/";
+        Player player = null;
+        GameObject tommyGun;
+        bool hasGunEquipped = false;
+        float timeSinceLastShot;
+        RaycastHit[] hits;
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        void Update()
+        {
+            if (player == null)
+            {
+                if (GameObject.Find("Player_Audrey(Clone)") != null)
+                {
+                    player = FindObjectOfType<Player>();
+                    Debug.Log("FOUND PLAYER");
+                    Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("TommyGunMod.Assets.tommygun");
+                    AssetBundle gunAsset = AssetBundle.LoadFromStream(str);
+
+                    tommyGun = Instantiate(gunAsset.LoadAsset<GameObject>("Tommy_Gun"));
+                    tommyGun.transform.SetParent(GameObject.Find(WeaponParentPath).transform);
+                    tommyGun.transform.localScale = new Vector3(100f, 100f, 100f);
+                    tommyGun.transform.localPosition = new Vector3(0.4f, -0.47f, 1.8f);
+                    tommyGun.transform.localEulerAngles = Vector3.zero;
+                    Debug.Log("Loaded Tommy Gun");
+                }
+            }
+            if (player != null)
+            {
+                timeSinceLastShot += Time.deltaTime;
+                if(player.AnimatorBody.GetInteger("HandWeaponStatus") == 0)
+                {
+                    tommyGun.SetActive(true);
+                }
+                else if(player.AnimatorBody.GetInteger("HandWeaponStatus") == 1)
+                {
+                    tommyGun.SetActive(false);
+                }
+                if (hasGunEquipped)
+                {
+                    if (Input.GetMouseButton(0))
+                    {
+                        if (timeSinceLastShot > 0.08f)
+                        {
+                            tommyGun.transform.GetChild(0).gameObject.SetActive(!tommyGun.transform.GetChild(0).gameObject.activeSelf);
+                            timeSinceLastShot = 0f;
+                            if (tommyGun.transform.GetChild(0).gameObject.activeSelf)
+                            {
+                                tommyGun.GetComponent<AudioSource>().Stop();
+                                tommyGun.GetComponent<AudioSource>().Play();
+                                hits = Physics.RaycastAll(player.GameCamera.transform.position, player.GameCamera.transform.forward);
+                                foreach (RaycastHit hit in hits)
+                                {
+                                    Transform objectHit = hit.transform;
+                                    Debug.Log(objectHit.gameObject.name);
+                                    if (objectHit.GetComponent<Enemy>() != null)
+                                    {
+                                        objectHit.GetComponent<Enemy>().OnHit(new RaycastHit());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (timeSinceLastShot > 0.08f)
+                            tommyGun.transform.GetChild(0).gameObject.SetActive(false);
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.L))
+                {
+                    Debug.Log("Swapping Weapons");
+                    if (hasGunEquipped)
+                    {
+                        player.ShowWeapon();
+                        //tommyGun.SetActive(false);
+                    }
+                    else
+                    {
+                        player.HideWeapon();
+                        //tommyGun.SetActive(true);
+                    }
+                    hasGunEquipped = !hasGunEquipped;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using TommyGunMod;
+// ...
+[assembly: MelonInfo(typeof(TommyGunMod.Main), "TommyGunMod", "1.0.0", "Mono")]
+[assembly: MelonGame("Joey Drew Studios", "Bendy and the Dark Revival")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TommyGunMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("N/A")]
+[assembly: AssemblyProduct("TommyGunMod")]
+[assembly: AssemblyCopyright("Copyright © N/A 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("13e4e763-52fb-49a3-8586-ae4ad2a6a5b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
